2565a2e5c264656765041cdb69a0cc652633fedc
yoshimo <109893020+yoshi-mo@users.noreply.github.com>
2023-09-08 08:20:14 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -10,15 +10,6 @@ const nextConfig = {
       "flagsapi.com",
       "res.cloudinary.com"
     ]
-  },
-  env:{
-    DATABASE_URL: process.env.DATABASE_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    GITHUB_ID: process.env.GITHUB_ID,
-    GITHUB_SECRET: process.env.GITHUB_SECRET,
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME
   }
 }
 
